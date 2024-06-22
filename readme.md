294da7ea5d56fb5f2df97cec351dbd6a7619a360
Jacob Zhang Jingwen <NewGuy3141@163.com>
2024-06-22 15:53:02 +0800

    First Upload

---
+++ readme.md
@@ -0,0 +1,3 @@
+The Omni Com MDK
+===
+This is a MDK (Maven Develop Kit) for [Omni Com](https://github.com/Sob1234509876/Omni-com) 
---
+++ src/java/top/example/Main.java
@@ -0,0 +1,17 @@
+package top.example;
+
+import org.apache.log4j.*;
+
+import top.sob.core.api.*;
+
+public class Main {
+
+    public static final Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+        LOGGER.info("Hello world!");
+        LOGGER.info("The report file URI: " + meta.REPORT_URI);
+
+    }
+}
